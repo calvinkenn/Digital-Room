75bc49cc7ba990bca0e3b6b33cd6d9ce81a45854
calvinkenn <calvink.palma@gmail.com>
2023-04-29 02:53:42 +0800

    Added mobile view

--- script.js
+++ script.js
@@ -55,6 +55,15 @@ function showMenu() {
   }
 }
 
+function showMobileMenu() {
+  const x = document.getElementById("mob");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   var lazyloadImages = document.querySelectorAll("img.lazy");
   var lazyloadThrottleTimeout;
